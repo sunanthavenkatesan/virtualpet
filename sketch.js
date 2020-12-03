978802dbe2d53e36fe1fbb66604e2baec29512fd
sunanthavenkatesan <54566675+sunanthavenkatesan@users.noreply.github.com>
2020-12-03 10:27:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,118 +1,81 @@
-//Create variables here
-var dogHappy,dogSitting,dog,database,foodstock,foods,position,button,hour,button2,milk;
-function preload()
-{
-  //load images here
-  dogHappy=loadImage('dogimg1.png')
-  dogSitting=loadImage('dogimg.png')
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var fedTime,lastFed;
+var feed,addFood;
+var foodObj;
+
+function preload(){
+sadDog=loadImage("Images/Dog.png");
+happyDog=loadImage("Images/happy dog.png");
 }
 
 function setup() {
-	createCanvas(500,500);
-database=firebase.database()
-foodStock = database.ref('milkbottles');
-  foodStock.on("value", readStock, showError)
-  dog=createSprite(250,250,40,40)
-  dog.addImage(dogSitting)
-  dog.scale=0.32
-  foods=20
- button=createButton('feed the dog')
- button2=createButton('add food')
- button2.position(150,100)
- button.position(100,100)
- milk=new Milk()
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+  
+  feed=createButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
 }
 
+function draw() {
+  background(46,139,87);
+  foodObj.display();
 
-function draw(){  
-  background(0)
-     button.mousePressed(change())
-     button2.mousePressed(plusstock(foods))
-   
-
-     gethour()
-
-  milk.display()
-
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+ 
+  fill(255,255,254);
+  textSize(15);
+  if(lastFed>=12){
+    text("Last Feed : "+ lastFed%12 + " PM", 350,30);
+   }else if(lastFed==0){
+     text("Last Feed : 12 AM",350,30);
+   }else{
+     text("Last Feed : "+ lastFed + " AM", 350,30);
+   }
+ 
   drawSprites();
-  //add styles here
-  /*if(foods>10){
-    text(7)
-    stroke('green')
-      text("milk remaining:"+foods,250,400)
-  }else{
-    text(7)
-    stroke('red')
-      text("milk remaining:"+foods,250,400)
-  }
-  text(7)
-    stroke('red')
-  text("note:press the key to feed the dog(key went down)",250,40)
-  */
 }
+
+//function to read food Stock
 function readStock(data){
-foods=data.val()
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
 }
-function writeStock(x){
-if(x<=0){
-x=0
-}else{
-  x=x-=1
-}
-database.ref('/').update({
-  foodleft:x
-  
-})
 
-console.log(x)
-}
-function plusstock(y){
-  if(y>=20){
-    y=20
-    }else{
-      y+=1
-    }
-    database.ref('/').update({
-      foodstock:y
-      
-    })
-    
-    console.log(y)
-}
-function showError(){
-  console.log('problems with the database')
-}
-async function gethour(){
-  if( button.mousePressed(change())){
-var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-  var responseJSON = await response.json();
-  var datetime = responseJSON.datetime;
-  
-     hour = datetime.slice(11,16);
-    var pm=hour.slice(0,3)
-    var unit
-    var ref=database.ref("lastfed")
-ref.on("value",some)
-some=hour.val()
-    if (pm>00&&pm<12) {
-      unit="AM"
-      text("last fed:"+hour+unit,50,100)
-    }else if(pm>12&&pm<=24)
-   unit="pm"
-   text("last fed:"+hour+unit,50,100)
 
-  }
+//function to update food stock and last fed time
+function feedDog(){
+  dog.addImage(happyDog);
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
   database.ref('/').update({
-    lastfed:hour.val()
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
   })
 }
-function change(){
-dog.changeImage(dogHappy)
-writeStock(foods)
-if (frameCount===frameCount+200) {
-  dog.changeImage(dogSitting)
-}
-database.ref('/').update({
-  
-})
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
 }
