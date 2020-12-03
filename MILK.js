42d386abfcb7828472cae017d738b4c9bdb5ddab
sunanthavenkatesan <54566675+sunanthavenkatesan@users.noreply.github.com>
2020-12-03 10:22:00 +0530

    Update MILK.js

--- MILK.js
+++ MILK.js
@@ -1,14 +1,40 @@
 class Milk{
     constructor(){
-        this.some=rectangle(80,100,7,7)
+    
+        this.some
+       
 this.image=loadImage('images/milk.png')
-this.foodstock=foodstock.val()
+    }
+
+ //add this        
+        
+        
+   updateFoodStock(foodStock){
+    this.foodStock=foodStock;
+   }
+
+   getFedTime(lastFed){
+     this.lastFed=lastFed;
+   }
+
+   deductFood(){
+     if(this.foodStock>0){
+      this.foodStock=this.foodStock-1;
+     }
+    }
+
+    getFoodStock(){
+      return this.foodStock;
+        
     }
     display(){
         var x=80
         var y=100
         imageMode(CENTER)
         image(this.image,80,100,7,7)
+        
+        
+        
 if(this.foodstock!=0){
 for(i=0;i<this.foodstock;i++){
 if(i%10===0){
