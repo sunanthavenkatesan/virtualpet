efaa7c7b78416be34b3c29417b6c3ea504198fa9
sunanthavenkatesan <54566675+sunanthavenkatesan@users.noreply.github.com>
2020-12-03 10:28:13 +0530

    Update MILK.js

--- MILK.js
+++ MILK.js
@@ -1,23 +1,43 @@
-class Milk{
+class Food {
     constructor(){
-        this.some=rectangle(80,100,7,7)
-this.image=loadImage('images/milk.png')
-this.foodstock=foodstock.val()
+    this.foodStock=0;
+    this.lastFed;
+    this.image=loadImage('Images/Milk.png');
     }
+
+   updateFoodStock(foodStock){
+    this.foodStock=foodStock;
+   }
+
+   getFedTime(lastFed){
+     this.lastFed=lastFed;
+   }
+
+   deductFood(){
+     if(this.foodStock>0){
+      this.foodStock=this.foodStock-1;
+     }
+    }
+
+    getFoodStock(){
+      return this.foodStock;
+    }
+
     display(){
-        var x=80
-        var y=100
-        imageMode(CENTER)
-        image(this.image,80,100,7,7)
-if(this.foodstock!=0){
-for(i=0;i<this.foodstock;i++){
-if(i%10===0){
-x=80;
-y=y+50
-}
-image(this.image,x,y,7,7)
-x=x+30
-}
-}
+      var x=80,y=100;
+      
+      imageMode(CENTER);
+      image(this.image,720,220,70,70);
+      
+      if(this.foodStock!=0){
+        for(var i=0;i<this.foodStock;i++){
+          if(i%10==0){
+            x=80;
+            y=y+50;
+          }
+          image(this.image,x,y,50,50);
+          x=x+30;
+        }
+      }
     }
 }
